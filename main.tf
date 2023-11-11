e9be9c7fc24c02adf5019fc51d75d950d8ecbaa5
Shyam <shyamneo24@gmail.com>
2023-11-11 16:22:28 +0530

    vpc

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "main" {
   instance_type             = var.instance_type
   vpc_security_group_ids    = [aws_security_group.main.id]
   subnet_id                 = var.subnet_ids[0]
-  tags                      = merge(local.tags, {Name = "${local.name_prefix}-rabbitmq"})
-  user_data                 = "${path.module}/userdata.sh"
+  tags                      = merge(local.tags, {Name = local.name_prefix })
+  user_data                 = file("${path.module}/userdata.sh")
 
 }

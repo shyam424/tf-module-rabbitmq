92e200d8d82b3638d775c0a79d254b71dd5d76b1
Shyam <shyamneo24@gmail.com>
2023-11-11 16:52:26 +0530

    vpc

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "main" {
 #creating dns record for rabbitmq
 resource "aws_route53_record" "main" {
   zone_id = var.zone_id
-  name    = "rabbitmq-$(var.env}"
+  name    = "rabbitmq-${var.env}"
   type    = "A"
   ttl     = 30
   records = [aws_instance.main.private_ip]
